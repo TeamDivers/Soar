a731b21c05aa8246a927f7347d6dd6f50bb1bc75
Jeon <gino9940@gmail.com>
2023-08-04 16:34:24 +0900

    chore: replace to eslint/import

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,15 +3,16 @@ module.exports = {
         browser: true,
         es2021: true
     },
-    extends: ['plugin:react/recommended', 'prettier'],
+    extends: ['plugin:react/recommended', 'prettier', 'plugin:import/recommended'],
     overrides: [],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module'
     },
-    plugins: ['react', '@typescript-eslint', 'prettier'],
+    plugins: ['react', '@typescript-eslint', 'prettier', 'import'],
     rules: {
-        'prettier/prettier': 'error'
+        'prettier/prettier': 'warn',
+        'import/no-unresolved': 'off'
     }
 };
