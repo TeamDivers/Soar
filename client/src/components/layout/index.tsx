0ee4ce6a66be2b612b6b6c68e68627b02f3f18e0
Jeon <gino9940@gmail.com>
2023-08-05 11:18:27 +0900

    comment: auth router implementation in layout

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
+    // if user is unauthorized, redirect to login
+
     return (
         <main className="container relative h-full max-w-md mx-auto bg-white">
             <Outlet />
