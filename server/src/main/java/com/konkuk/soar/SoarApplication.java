d26ccd9a28488e0c5f68c08c6c16446cf0825085
jaemin <anjm1020@gmail.com>
2023-08-07 06:05:36 +0900

    Feat : Server 프로젝트 초기화

---
+++ server/src/main/java/com/konkuk/soar/SoarApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.soar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoarApplication.class, args);
+	}
+
+}
