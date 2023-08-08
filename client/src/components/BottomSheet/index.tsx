2f20a13a8579ea3dce51e7a0787b52189c62d044
Jeon <gino9940@gmail.com>
2023-08-08 21:55:22 +0900

    feat: add BottomSheet component

---
+++ client/src/components/BottomSheet/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Sheet from 'react-modal-sheet';
+
+import '@styles/sheet.css';
+
+interface BottomSheetProps {
+    isOpen: boolean;
+    onClose: () => void;
+    toggle: () => void;
+    snapPoint?: number;
+    children: React.ReactNode;
+}
+
+const BottomSheet = ({
+    isOpen,
+    onClose,
+    toggle,
+    snapPoint = 0.8,
+    children
+}: BottomSheetProps) => {
+    return (
+        <Sheet
+            isOpen={isOpen}
+            onClose={onClose}
+            snapPoints={[snapPoint, 0]}
+            disableDrag
+        >
+            <Sheet.Container>
+                <Sheet.Header />
+                <Sheet.Content>
+                    <div className="px-10">{children}</div>
+                </Sheet.Content>
+            </Sheet.Container>
+            <Sheet.Backdrop onTap={toggle} />
+        </Sheet>
+    );
+};
+
+export default BottomSheet;
