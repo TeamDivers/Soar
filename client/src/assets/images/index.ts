ef5e76b0c0d9f087878149e4ac33c3672e4f50e1
Jeon <gino9940@gmail.com>
2023-08-11 01:08:31 +0900

    feat: add svg export index file

---
+++ client/src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as Home } from './home.svg';
+export { ReactComponent as Portfolio } from './portfolio.svg';
+export { ReactComponent as Ranking } from './ranking.svg';
+export { ReactComponent as Record } from './record.svg';
