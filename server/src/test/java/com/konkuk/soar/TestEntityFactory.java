615c25e893b141f543f47d76682892066eb17ef6
jaemin <anjm1020@gmail.com>
2023-08-11 00:45:38 +0900

    Fix : 테스트 엔티티팩토리 수정 및 gradle 변경

--- server/src/test/java/com/konkuk/soar/TestEntityFactory.java
+++ server/src/test/java/com/konkuk/soar/TestEntityFactory.java
@@ -4,6 +4,9 @@ import com.konkuk.soar.common.domain.File;
 import com.konkuk.soar.common.domain.Tag;
 import com.konkuk.soar.member.domain.Member;
 import com.konkuk.soar.portfolio.domain.Portfolio;
+import com.konkuk.soar.portfolio.domain.PortfolioBookmark;
+import com.konkuk.soar.portfolio.domain.PortfolioReview;
+import com.konkuk.soar.portfolio.domain.PortfolioTag;
 import com.konkuk.soar.portfolio.domain.project.Project;
 import com.konkuk.soar.studyhistory.domain.StudyHistory;
 
@@ -11,7 +14,13 @@ import java.time.LocalDateTime;
 
 public class TestEntityFactory {
 
-    static Portfolio portfolio(Member member) {
+    public static Member member() {
+        return Member.builder()
+                .name("member")
+                .build();
+    }
+
+    public static Portfolio portfolio(Member member) {
         return Portfolio.builder()
                 .member(member)
                 .isPublic(true)
@@ -21,7 +30,32 @@ public class TestEntityFactory {
                 .build();
     }
 
-    static Project project(Portfolio portfolio) {
+    public static PortfolioBookmark portfolioBookmark(Member member, Portfolio portfolio) {
+        return PortfolioBookmark.builder()
+                .member(member)
+                .portfolio(portfolio)
+                .build();
+    }
+
+    public static PortfolioReview portfolioReview(Member member,Portfolio portfolio) {
+        return PortfolioReview.builder()
+                .comment("comment")
+                .differenceScore(4.0F)
+                .expertiseScore(4.0F)
+                .perfectionScore(4.0F)
+                .member(member)
+                .portfolio(portfolio)
+                .build();
+    }
+
+    public static PortfolioTag portfolioTag(Tag tag, Portfolio portfolio) {
+        return PortfolioTag.builder()
+                .tag(tag)
+                .portfolio(portfolio)
+                .build();
+    }
+
+    public static Project project(Portfolio portfolio) {
         return Project.builder()
                 .portfolio(portfolio)
                 .title("title")
@@ -34,7 +68,7 @@ public class TestEntityFactory {
                 .build();
     }
 
-    static StudyHistory studyHistory(Member member) {
+    public static StudyHistory studyHistory(Member member) {
         return StudyHistory.builder()
                 .member(member)
                 .startDate(LocalDateTime.now())
@@ -46,7 +80,7 @@ public class TestEntityFactory {
                 .build();
     }
 
-    static File file() {
+    public static File file() {
         return File.builder()
                 .url("url")
                 .type("type")
@@ -55,7 +89,7 @@ public class TestEntityFactory {
                 .build();
     }
 
-    static Tag tag() {
+    public static Tag tag() {
         return Tag.builder()
                 .name("name")
                 .build();
