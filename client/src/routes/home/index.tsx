97157fbe05ed9d57584b667b4a900975823355e0
Jeon <gino9940@gmail.com>
2023-08-11 16:07:15 +0900

    feat: add custom boxShadow attribute

--- client/src/routes/home/index.tsx
+++ client/src/routes/home/index.tsx
@@ -8,7 +8,13 @@ import useBottomSheet from '@hooks/useBottomSheet';
 const Home = () => {
     const { isOpen, toggle, close } = useBottomSheet();
 
-    return <div className=""></div>;
+    return (
+        <div className="flex flex-col pt-8">
+            <div className="bg-white rounded-[20px] pt-10 px-[26px] shadow-md pb-4">
+                Hi
+            </div>
+        </div>
+    );
 };
 
 export default Home;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ const colors = require('tailwindcss/colors');
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                md: '0px 0px 10px 0px rgba(0, 0, 0, 0.10);'
+            }
+        },
         colors: {
             ...colors,
             primary: '#1D5CFF'
