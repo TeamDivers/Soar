ee1b3f8f6f3afe172569da6f7d6e0c74d225aaf3
Jeon <gino9940@gmail.com>
2023-08-12 07:01:44 +0900

    feat: add variant of RoundButton

--- client/src/components/RoundButton/index.tsx
+++ client/src/components/RoundButton/index.tsx
@@ -2,13 +2,39 @@ import React from 'react';
 
 interface RoundButtonProps {
     children: React.ReactNode;
+    onClick: () => void;
+    backgroundColor?: string;
+    textColor?: string;
+    hasBorder?: boolean;
+    borderColor?: string;
 }
 
-const RoundButton = ({ children }: RoundButtonProps) => {
+const RoundButton = ({
+    children,
+    onClick,
+    backgroundColor = 'primary',
+    textColor = 'white',
+    hasBorder,
+    borderColor = '#DDDDDD'
+}: RoundButtonProps) => {
     return (
-        <div className="bg-primary rounded-[50px] flex justify-center items-center py-[14px]">
-            <span className="text-base font-bold text-white">{children}</span>
-        </div>
+        <button
+            type="button"
+            onClick={onClick}
+            className={`bg-primary rounded-[50px] flex justify-center w-full items-center py-[14px]`}
+            style={{
+                backgroundColor,
+                borderColor,
+                borderWidth: hasBorder ? 1 : 0
+            }}
+        >
+            <span
+                className={`text-base font-bold text-white`}
+                style={{ color: textColor }}
+            >
+                {children}
+            </span>
+        </button>
     );
 };
 
