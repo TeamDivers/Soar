4ef7444f4b960e133969929e3e05d94979199511
Jeon <gino9940@gmail.com>
2023-08-11 19:16:31 +0900

    feat: add pencil svg

--- client/src/assets/images/index.ts
+++ client/src/assets/images/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as Portfolio } from './portfolio.svg';
 export { ReactComponent as Ranking } from './ranking.svg';
 export { ReactComponent as Record } from './record.svg';
 export { ReactComponent as Logo } from './logo.svg';
+export { ReactComponent as Pencil } from './pencil.svg';
