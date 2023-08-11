fc108485127b143df03dc766ee3ac81e6057d9e4
Jeon <gino9940@gmail.com>
2023-08-12 07:01:24 +0900

    feat: add sns login assets

--- client/src/assets/images/index.ts
+++ client/src/assets/images/index.ts
@@ -4,3 +4,5 @@ export { ReactComponent as Ranking } from './ranking.svg';
 export { ReactComponent as Record } from './record.svg';
 export { ReactComponent as Logo } from './logo.svg';
 export { ReactComponent as Pencil } from './pencil.svg';
+export { ReactComponent as Google } from './Google.svg';
+export { ReactComponent as Kakao } from './Kakao.svg';
