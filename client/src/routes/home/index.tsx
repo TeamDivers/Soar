fe3783f7972fe70fc3fc1c1ee5d05a72871aba3b
Jeon <gino9940@gmail.com>
2023-08-11 19:17:24 +0900

    feat: implement home partially

--- client/src/routes/home/index.tsx
+++ client/src/routes/home/index.tsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import BottomSheet from '@components/BottomSheet';
+import Divider from '@components/Divider';
+import RoundButton from '@components/RoundButton';
 
-import useBottomSheet from '@hooks/useBottomSheet';
+import { Pencil } from '../../assets/images';
 
 const Home = () => {
-    const { isOpen, toggle, close } = useBottomSheet();
-
     return (
         <div className="flex flex-col pt-8">
             <div className="bg-white rounded-[20px] pt-10 px-[26px] shadow-md pb-4">
-                Hi
+                <div className="flex justify-between pb-[22px]">
+                    <div>
+                        <div className="pb-2 text-lg font-bold text-black">
+                            전진호님이
+                            <br />
+                            만들 수 있는 포트폴리오가
+                            <br />
+                            4개 이상이에요!
+                        </div>
+                        <RoundButton>포트폴리오 만들기</RoundButton>
+                    </div>
+                    <Pencil className="w-[100px]" />
+                    {/* TODO: size of pencil logo will be change!! */}
+                </div>
+                <Divider isFull />
+                <div className="flex justify-around itmes-center pt-[14px]">
+                    <div className="flex flex-col items-center">
+                        <span className="text-xs font-medium text-zinc-500">
+                            내 학습기록
+                        </span>
+                        <span className="text-lg font-bold text-blue-600">
+                            4
+                        </span>
+                    </div>
+                    <Divider direction="vertical" length={30} />
+                    <div className="flex flex-col items-center ">
+                        <span className="text-xs font-medium text-zinc-500">
+                            보유 포트폴리오
+                        </span>
+                        <span className="text-lg font-bold text-blue-600">
+                            7
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     );
