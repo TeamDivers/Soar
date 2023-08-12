1cb05e03b52dcf36baa3db4d49a2a58cbe5b2b0b
Jeon <gino9940@gmail.com>
2023-08-12 21:01:06 +0900

    feat: add chevron-down svg

--- client/src/assets/images/index.ts
+++ client/src/assets/images/index.ts
@@ -7,4 +7,5 @@ export { ReactComponent as Pencil } from './pencil.svg';
 export { ReactComponent as Google } from './Google.svg';
 export { ReactComponent as Kakao } from './Kakao.svg';
 export { ReactComponent as Left } from './chevron-left.svg';
+export { ReactComponent as Down } from './chevron-down.svg';
 export { ReactComponent as Delete } from './delete.svg';
