6d03b6c612e412a9737e5d2a1a3f0d9bc11e7e33
Jeon <gino9940@gmail.com>
2023-08-14 02:27:08 +0900

    feat: add custom modal

---
+++ client/src/components/Modal/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { default as ReactModal } from 'react-modal';
+
+interface ModalProps {
+    isOpen: boolean;
+    close: () => void;
+    children: React.ReactNode;
+}
+
+const customStyles = {
+    overlay: { background: 'rgba(0, 0, 0, 0.40)', backdropFilter: 'blur(3px)' },
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        border: 'none',
+        padding: 0,
+        background: 'none'
+        // position: absolute;
+        // inset: 50% auto auto 50%;
+        // border: none;
+        // background: rgb(255, 255, 255);
+        // overflow: auto;
+        // border-radius: 4px;
+        // outline: none;
+        // padding: 20px;
+        // margin-right: -50%;
+        // transform: translate(-50%, -50%);
+    }
+};
+
+const Modal = ({ isOpen, close, children }: ModalProps) => {
+    return (
+        <ReactModal
+            isOpen={isOpen}
+            onRequestClose={close}
+            style={customStyles}
+            contentLabel="Example Modal"
+        >
+            {children}
+        </ReactModal>
+    );
+};
+
+export default Modal;
