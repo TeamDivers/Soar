49f9435758e50b8ab523e5ca6ebf456fe673f3f1
Jeon <gino9940@gmail.com>
2023-08-13 10:44:34 +0900

    feat: add getRecords

---
+++ client/src/apis/record/getRecords.ts
@@ -0,0 +1,13 @@
+export interface RecordType {
+    category: string;
+    startDate: string;
+    endDate: string;
+}
+
+const getRecords = (): RecordType[] => {
+    return [];
+};
+
+const useGetRecords = () => {};
+
+export default useGetRecords;
