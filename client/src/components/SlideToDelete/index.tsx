ae2280b8dc1cc1317761c688cc9ff258d021289c
Jeon <gino9940@gmail.com>
2023-08-15 03:08:05 +0900

    fix: limit swipe area

--- client/src/components/SlideToDelete/index.tsx
+++ client/src/components/SlideToDelete/index.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Swiper, SwiperSlide, useSwiper, useSwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
 import './index.css';
 import { Delete } from '@images/index';
+
+import { useIntersectionObserver } from '@hooks/useIntersectionObserver';
 
 interface SlideToDeleteProps {
     onDelete: () => void;
@@ -15,9 +17,14 @@ const SlideToDelete: React.FC<SlideToDeleteProps> = ({
     children
 }) => {
     return (
-        <Swiper centeredSlides={true}>
-            <SwiperSlide>{children}</SwiperSlide>
-            <SwiperSlide>
+        <Swiper
+            spaceBetween={0}
+            resistance={true}
+            resistanceRatio={0}
+            slidesPerView={'auto'}
+        >
+            <SwiperSlide className="first-slide">{children}</SwiperSlide>
+            <SwiperSlide className="last-slide">
                 <DeleteSlide onDelete={onDelete} />
             </SwiperSlide>
         </Swiper>
@@ -25,18 +32,40 @@ const SlideToDelete: React.FC<SlideToDeleteProps> = ({
 };
 
 const DeleteSlide = ({ onDelete }: { onDelete: () => void }) => {
-    const swiperSlide = useSwiperSlide();
     const swiper = useSwiper();
 
+    const ref = useRef<HTMLDivElement | null>(null);
+    const entry = useIntersectionObserver(ref, {
+        threshold: 0.4
+    });
+    const [isVisible, setIsVisible] = useState(false);
+
+    let timer: NodeJS.Timeout;
+
     useEffect(() => {
-        if (swiperSlide.isActive) {
+        if (entry?.isIntersecting) {
+            timer = setTimeout(() => {
+                setIsVisible(true);
+            }, 800);
+        } else {
+            if (timer) clearTimeout(timer);
+            setIsVisible(false);
+        }
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [entry?.isIntersecting]);
+
+    useEffect(() => {
+        if (isVisible) {
             swiper.slidePrev();
             onDelete();
         }
-    }, [swiperSlide.isActive]);
+    }, [isVisible]);
 
     return (
-        <div className="h-full py-2">
+        <div ref={ref} className="h-full py-2">
             <button className="flex items-center justify-center h-full px-4 rounded-[10px] bg-red-600">
                 <Delete />
             </button>
