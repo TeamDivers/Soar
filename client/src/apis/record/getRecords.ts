f85db92ef39a482f4219a2dbd5fa9c243951ceed
Jeon <gino9940@gmail.com>
2023-08-14 22:04:59 +0900

    feat: impl getRecord with react-query

--- client/src/apis/record/getRecords.ts
+++ client/src/apis/record/getRecords.ts
@@ -1,13 +1,26 @@
+import { useQuery } from 'react-query';
+
+import { request } from '../axios';
+
 export interface RecordType {
+    id: number;
+    type: string;
+    content: string;
+    memberId: number;
     category: string;
     startDate: string;
     endDate: string;
 }
 
-const getRecords = (): RecordType[] => {
-    return [];
+const getRecords = () => {
+    return request<RecordType[]>({
+        method: 'GET',
+        url: '/studyhistories'
+    });
 };
 
-const useGetRecords = () => {};
+const useGetRecords = () => {
+    return useQuery(['records'], () => getRecords());
+};
 
 export default useGetRecords;
