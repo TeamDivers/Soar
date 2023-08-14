07ce7a90f739f23f663294f3ff132228e61c6918
jaemin <anjm1020@gmail.com>
2023-08-14 12:58:55 +0900

    fix: embedded db에서 local db로 테스트 컨텍스트 변경

--- server/src/test/java/com/konkuk/soar/portfolio/dto/project/response/ProjectResponseDtoTest.java
+++ server/src/test/java/com/konkuk/soar/portfolio/dto/project/response/ProjectResponseDtoTest.java
@@ -32,7 +32,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class ProjectResponseDtoTest {
 
   @Autowired
