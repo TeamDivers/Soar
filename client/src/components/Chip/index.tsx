5c7305736083f2ad3113612d7f6dc8c755cd6ee4
Jeon <gino9940@gmail.com>
2023-08-15 23:19:05 +0900

    fix: chip font style

--- client/src/components/Chip/index.tsx
+++ client/src/components/Chip/index.tsx
@@ -8,13 +8,13 @@ interface ChipProps {
 
 const Chip: React.FC<ChipProps> = ({ label, isSelected = false, onClick }) => {
     const chipClasses = isSelected
-        ? 'bg-primary text-white'
-        : 'border border-stone-300 bg-white';
+        ? 'bg-primary text-white text-white text-sm font-semibold'
+        : 'border border-stone-300 bg-white text-zinc-500 text-sm font-medium';
 
     return (
         <button
             onClick={onClick}
-            className={`chip inline-flex items-center px-3 py-1 m-1 rounded-full ${chipClasses} min-w-fit`}
+            className={`chip inline-flex items-center px-3 py-1 m-1 rounded-full ${chipClasses} min-w-fit `}
         >
             {label}
         </button>
