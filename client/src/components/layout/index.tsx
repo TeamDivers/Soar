acb9b7eaf67a0918f989285b2a15e4585885daef
Jeon <gino9940@gmail.com>
2023-08-15 19:30:23 +0900

    feat: add title, right props to layout

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -10,12 +10,16 @@ interface LayoutProps {
     hasHeader?: boolean;
     hasProfile?: boolean;
     children?: React.ReactNode;
+    title?: string;
+    right?: React.ReactNode;
 }
 
 const Layout = ({
     hasNavigation = true,
     hasHeader = true,
     hasProfile = false,
+    title,
+    right = <></>,
     children
 }: LayoutProps) => {
     // if user is unauthorized, redirect to login
@@ -24,12 +28,14 @@ const Layout = ({
         <main className={`container relative h-full max-w-md mx-auto bg-white`}>
             {hasHeader && (
                 <div className="flex items-center justify-between h-10 px-5 pt-3">
-                    <Logo className="w-16" />
-                    {hasProfile && (
+                    {title && <Logo className="w-16" />}
+                    {hasProfile ? (
                         <img
                             src="https://placehold.co/400"
                             className="w-10 h-10 rounded-full"
                         />
+                    ) : (
+                        right
                     )}
                 </div>
             )}
