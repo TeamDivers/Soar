6daef2158ee1581195d7de921cb2e6cbf7c91fc7
Jeon <gino9940@gmail.com>
2023-08-17 08:28:14 +0900

    fix: pencil icon

--- client/src/routes/home/index.tsx
+++ client/src/routes/home/index.tsx
@@ -1,33 +1,47 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { useGetPortfolios } from '@apis/portfolio/getPortfolio';
+import { useGetRecords } from '@apis/record/getRecords';
+
 import Divider from '@components/Divider';
 import RoundButton from '@components/RoundButton';
 
 import { Pencil } from '@images/index';
 
+import { getMemberId } from '@utils/auth';
+
 const Home = () => {
     const navigate = useNavigate();
+
+    const { data: records } = useGetRecords({
+        memberId: parseInt(getMemberId())
+    });
+    const { data: portfolios } = useGetPortfolios({
+        memberId: parseInt(getMemberId())
+    });
 
     return (
         <div className="flex flex-col px-4 pt-5">
             <div className="bg-white rounded-[20px] pt-10 px-[26px] shadow-md pb-4">
                 <div className="flex justify-between pb-[22px]">
                     <div>
-                        <div className="pb-2 text-lg font-bold text-black">
+                        <div className="pb-[17px] text-lg font-normal text-black aggro leading-[21.58px]">
                             전진호님이
                             <br />
                             만들 수 있는 포트폴리오가
                             <br />
-                            4개 이상이에요!
+                            {records?.length}개 이상이에요!
                         </div>
                         <RoundButton
                             onClick={() => navigate('/portfolio/create')}
                         >
-                            포트폴리오 만들기
+                            <div className="leading-[19.09px] py-[12px] text-white text-base font-bold">
+                                포트폴리오 만들기
+                            </div>
                         </RoundButton>
                     </div>
-                    <Pencil className="w-[100px]" />
+                    <Pencil className="w-[144px]" />
                     {/* TODO: size of pencil logo will be change!! */}
                 </div>
                 <Divider isFull />
@@ -37,7 +51,7 @@ const Home = () => {
                             내 학습기록
                         </span>
                         <span className="text-lg font-bold text-blue-600">
-                            4
+                            {records?.length}
                         </span>
                     </div>
                     <Divider direction="vertical" length={30} />
@@ -46,7 +60,7 @@ const Home = () => {
                             보유 포트폴리오
                         </span>
                         <span className="text-lg font-bold text-blue-600">
-                            7
+                            {portfolios?.length}
                         </span>
                     </div>
                 </div>
