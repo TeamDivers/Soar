a32d7ebc66f312a83ef98f3d9922f4d875fce9cc
Jeon <gino9940@gmail.com>
2023-08-17 03:02:50 +0900

    feat: replace dummy to props

--- client/src/components/RecordCard/index.tsx
+++ client/src/components/RecordCard/index.tsx
@@ -1,20 +1,28 @@
+import moment from 'moment';
 import React from 'react';
 
 import Dot from '@components/Dot';
 
 import { text2Color } from '@utils/color';
 
-interface RecordCardProps {}
+import { RecordType } from '@interfaces/record';
 
-const RecordCard = ({}: RecordCardProps) => {
+interface RecordCardProps {
+    record: RecordType;
+}
+
+const RecordCard = ({ record }: RecordCardProps) => {
     return (
         <div className="flex items-center justify-between w-full shadow-md rounded-[10px] py-[18px] px-4">
             <div className="flex items-center gap-3">
-                <Dot color={text2Color('수학')} size={24} />
-                <span className="text-base font-semibold text-black">수학</span>
+                <Dot color={text2Color(record.tagName)} size={24} />
+                <span className="text-base font-semibold text-black">
+                    {record.tagName}
+                </span>
             </div>
             <span className="text-sm font-medium text-neutral-400">
-                2023.08.08 8:00-14:00
+                {moment(record.startDate).format('YYYY.MM.DD H:MM')}-
+                {moment(record.endDate).format('H:MM')}
             </span>
         </div>
     );
