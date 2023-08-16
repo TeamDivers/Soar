28f404a42478a1e2198174d4dd052b84f752ed35
Jeon <gino9940@gmail.com>
2023-08-16 20:22:15 +0900

    feat: add portfolio type

---
+++ client/src/types/portfolio.ts
@@ -0,0 +1,57 @@
+interface PortfolioType {
+    portfolioId: number;
+    memberId: number;
+    title: string;
+    description: string;
+    category: string;
+    bookmark: number;
+    rank: number;
+    score: number;
+    projects: Project[];
+    reviews: Review[];
+    tags: Tags;
+}
+
+interface Tags {
+    name: string[];
+}
+
+interface Review {
+    memberId: number;
+    portfolioId: number;
+    expertiseScore: number;
+    differenceScore: number;
+    perfectionScore: number;
+    comment: string;
+}
+
+interface Project {
+    projectId: number;
+    portfolioId: number;
+    title: string;
+    category: string;
+    role: string;
+    description: string;
+    startDate: string;
+    endDate: string;
+    studyHistories: StudyHistory[];
+    files: File[];
+}
+
+interface File {
+    fileId: number;
+    type: string;
+    fileName: string;
+    url: string;
+}
+
+interface StudyHistory {
+    id: number;
+    content: string;
+    memberId: number;
+    tagName: string;
+    startDate: string;
+    endDate: string;
+}
+
+export type { PortfolioType, Tags, Review, Project, File, StudyHistory };
