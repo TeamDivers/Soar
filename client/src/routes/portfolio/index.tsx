c7d99a465a6f2441f69794cd0337cbfe6ecf05e2
Jeon <gino9940@gmail.com>
2023-08-17 04:15:23 +0900

    feat: add useGetPortfolio

--- client/src/routes/portfolio/index.tsx
+++ client/src/routes/portfolio/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { useGetPortfolios } from '@apis/portfolio/getPortfolio';
+
 import PortfolioCard from '@components/PortfolioCard';
 import SlideToDelete from '@components/SlideToDelete';
 import SortSelector from '@components/SortSelector';
@@ -9,7 +11,8 @@ import { Plus } from '@images/index';
 
 const Portfolio = () => {
     const navigate = useNavigate();
-    const portfolios = [{ id: 1 }];
+
+    const { data: portfolios } = useGetPortfolios({ memberId: 1 });
 
     const onDeletePortfolio = () => {};
 
@@ -18,19 +21,19 @@ const Portfolio = () => {
     };
 
     return (
-        <>
+        <div className="overflow-y-scroll">
             <div className="flex justify-between px-4 mt-[34px] mb-2">
                 <h1 className="text-xl font-bold text-black">
                     나의 포트폴리오
                 </h1>
                 <SortSelector onChange={(v: string) => console.log(v)} />
             </div>
-            <div>
-                {portfolios.map((portfolio) => {
+            <div className="">
+                {portfolios?.map((portfolio) => {
                     return (
                         <SlideToDelete
                             onDelete={onDeletePortfolio}
-                            key={portfolio.id}
+                            key={portfolio.portfolioId}
                         >
                             <div className="px-4 py-2">
                                 <PortfolioCard />
@@ -40,7 +43,7 @@ const Portfolio = () => {
                 })}
             </div>
             <div
-                className="flex fixed bottom-[118px] max-w-md justify-end pr-4"
+                className="flex fixed bottom-[118px] max-w-md justify-end pr-4 z-50"
                 style={{ width: '-webkit-fill-available' }}
             >
                 <button
@@ -51,7 +54,7 @@ const Portfolio = () => {
                     <Plus />
                 </button>
             </div>
-        </>
+        </div>
     );
 };
 
