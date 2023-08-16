c8531199c59d746474037c2afc4a21a42be94b50
Jeon <gino9940@gmail.com>
2023-08-17 07:09:20 +0900

    feat: impl record create page

--- client/src/routes/record/create/index.tsx
+++ client/src/routes/record/create/index.tsx
@@ -5,8 +5,10 @@ import { useCreateRecord } from '@apis/record/createRecord';
 import { Title } from '@routes/portfolio/create';
 
 import CreateNavigation from '@components/CreateNavigation';
+import DateTimePicker from '@components/DateTimePicker';
 import Divider from '@components/Divider';
 import FileInput from '@components/FileInput';
+import TextArea from '@components/TextArea';
 import TextInput from '@components/TextInput';
 
 import useTextInput from '@hooks/useTextInput';
@@ -16,8 +18,12 @@ const RecordCreate = () => {
 
     const { mutate } = useCreateRecord();
 
+    const content = useTextInput();
+    const category = useTextInput();
     const { value: title, onChange: onChangeTitle } = useTextInput();
     const [file, setFile] = useState<File>();
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
 
     const handleOnSave = () => {
         if (file) {
@@ -31,10 +37,10 @@ const RecordCreate = () => {
 
     return (
         <>
-            <div className="flex flex-col justify-between px-4 pt-[34px] mb-2 overflow-y-scroll h-screen pb-[100px]">
+            <div className="flex flex-col justify-start px-4 pt-[34px] mb-2 overflow-y-scroll h-screen pb-[100px]">
                 {/* portfolio title section */}
-                <div className="my-[40px]">
-                    <Title text="포트폴리오 제목" />
+                <div className="mb-[40px]">
+                    <Title text="제목" />
                     <TextInput
                         value={title}
                         onChange={onChangeTitle}
@@ -42,19 +48,56 @@ const RecordCreate = () => {
                     />
                 </div>
                 <Divider />
-                {/* 썸네일 추가 */}
+                {/* 날짜 및 시간 추가 */}
+                <div className="my-5">
+                    <Title text={'날짜 및 시간'} />
+                    <div className="flex justify-between gap-[6px] items-center">
+                        <DateTimePicker
+                            value={startDate}
+                            onChange={setStartDate}
+                        />
+                        <span className="text-base font-medium text-neutral-500">
+                            ~
+                        </span>
+                        <DateTimePicker value={endDate} onChange={setEndDate} />
+                    </div>
+                </div>
+                <Divider />
+                {/* 공부 내용 */}
                 <section>
                     <div className="my-[40px]">
-                        <Title text="썸네일 추가" isRequired={false} />
+                        <Title text="공부 내용" isRequired={false} />
+                        <TextArea
+                            value={content.value}
+                            placeholder="공부에 대한 내용을 입력해주세요."
+                            onChange={content.onChange}
+                        />
+                    </div>
+                </section>
+                <Divider />
+                {/* 카테고리 입력 */}
+                <section>
+                    <div className="my-[40px]">
+                        <Title text="카테고리 입력" isRequired={false} />
+                        <TextInput
+                            value={category.value}
+                            onChange={category.onChange}
+                        />
+                    </div>
+                </section>
+                <Divider />
+                {/* 기타 파일 추가 */}
+                <section>
+                    <div className="my-[40px]">
+                        <Title text="기타 파일 추가" isRequired={false} />
                         <div className="flex flex-col gap-2 mb-2">
                             <FileInput
-                                label={'이미지'}
+                                label={'파일'}
                                 onChange={handleOnChangeFileInput}
                             />
                         </div>
                     </div>
                 </section>
-                <Divider />
             </div>
             <CreateNavigation
                 onCancel={function (): void {
