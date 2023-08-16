14d7351ec455e33648999890c4af98c2092bf710
Jeon <gino9940@gmail.com>
2023-08-16 19:34:49 +0900

    fix: api base url

--- client/src/apis/axios.ts
+++ client/src/apis/axios.ts
@@ -9,7 +9,7 @@ export interface Response<T> {
 }
 
 const instance = axios.create({
-    baseURL: `${process.env.API_BASE_URI}`,
+    baseURL: `${process.env.REACT_APP_API_BASE_URI}`,
     timeout: 3000
 });
 
